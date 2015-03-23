8e73c7fc76988a8137796a44da5a4f6f46a498f5
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-03-23 22:22:24 +0530

    images added

--- first-extension v0.2/script.js
+++ first-extension v0.2/script.js
@@ -1,11 +1,44 @@
-document.getElementById("button1").addEventListener("click",clickHandler);
+window.addEventListener("load", function () {
+	// chrome.storage.get('value',function(val){
+		// console.log(chrome.storage)
+		if(localStorage["mysetting"]){
+			document.getElementById("input_box_1").value = localStorage["mysetting"];
+		}
+	// });
+	// document.getElementById("input_box_1").value = ;
+});
 
-function clickHandler () {
-	console.log('clickHandler 2 ');
-	chrome.notifications.create('hi',{
-		type: "basic",
-		title: "Alert",
-		message: "You have clicked on my extension.",
-		iconUrl: "icon128.png"
-	},function(){});
+document.getElementById("save-btn").addEventListener("click",storeTheData);
+document.getElementById("clear-btn").addEventListener("click",clearTheData);
+
+function clearTheData () {
+	localStorage["mysetting"] = "";
+	document.getElementById("input_box_1").value = localStorage["mysetting"];
+}
+
+function storeTheData() {
+	var ipVal = document.getElementById("input_box_1").value;
+	
+	if (!ipVal) {
+		chrome.notifications.create('Warning!',{
+			type: "basic",
+			title: "Alert",
+			message: "Please enter some value.",
+			iconUrl: "warning_icon_64.png"
+		},function(){});
+		return;
+	}
+	// Save it using the Chrome extension storage API.
+	localStorage["mysetting"] = ipVal;
+
+	// chrome.storage.sync.set({'value': ipVal}, function() {
+
+		chrome.notifications.create('Success!',{
+			type: "basic",
+			title: "Alert",
+			message: "Data saved successfully.",
+			iconUrl: "success_icon_64.png"
+		},function(){});
+		// console.log(chrome.storage)
+	// });
 }
