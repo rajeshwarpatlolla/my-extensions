7ed43e3ea5cf53c39a28f4eb1591d1ab6ed3edb2
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-03-24 22:19:18 +0530

    add, remove functionality added.

--- first-extension v0.3/script.js
+++ first-extension v0.3/script.js
@@ -3,12 +3,48 @@ var app = angular.module('myExtension', []);
 app.controller('MainController', function($scope) {
 
 	$scope.reminders = [
-		{name:'1', message:'msg 1', time:'9am'},
-		{name:'2', message:'msg 2', time:'10am'},
-		{name:'3', message:'msg 3', time:'11am'},
-		{name:'4', message:'msg 4', time:'12pm'},
-		{name:'5', message:'msg 5', time:'1pm'}
+	{id:1,message:'This is 1st message from RR', time:'9am'},
+	{id:2,message:'This is 2nd message from RR', time:'10am'},
+	{id:3,message:'This is 3rd message from RR', time:'11am'},
+	{id:4,message:'This is 4th message from RR', time:'12pm'},
+	{id:5,message:'This is 5th message from RR', time:'1pm'}
 	];
+
+	$scope.addNewReminderVal = false;
+	$scope.addNewReminder = function(){
+		$scope.addNewReminderVal = true;
+	};
+	
+	$scope.submitNewReminder = function(newReminder){
+		var highestId = 0;
+		angular.forEach($scope.reminders, function(val, key){
+			if(highestId < val.id){
+				highestId = val.id;
+			}
+		});
+		$scope.reminders.push({id:highestId+1,message:newReminder.description,time:newReminder.time});
+		$scope.addNewReminderVal = false;
+	};
+
+	$scope.cancel = function(){
+		$scope.addNewReminderVal = false;
+	};
+
+	$scope.editReminder = function(reminder){
+
+	};
+
+	$scope.removeReminder = function(reminder){
+		var index = 0, count = 0;
+		angular.forEach($scope.reminders, function(val, key){
+			if(val.id === reminder.id){
+				index = count;
+			}
+			count += 1;
+		});
+
+		$scope.reminders.splice(index,1);
+	};
 
 	function getNotification (type, msg, img) {
 		chrome.notifications.create(type,{
