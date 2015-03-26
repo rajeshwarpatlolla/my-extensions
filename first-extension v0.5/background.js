ad88e402be0de6ee110c2575c66450c73dbb844b
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-03-26 23:05:46 +0530

    changes in bg pages

--- first-extension v0.5/background.js
+++ first-extension v0.5/background.js
@@ -1,11 +1,10 @@
 window.addEventListener("load", clickHandler);
 
 function clickHandler () {
-	console.log('clickHandler 1 ');
 	chrome.notifications.create('',{
 		type: "basic",
 		title: "PriceRadar",
 		message: "Item added successfully bg",
-		iconUrl: "icon.png"
+		iconUrl: "icon-48.png"
 	},function(){});
 }
