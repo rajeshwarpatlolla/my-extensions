632dcca97faee69e49c7c13e9eff2695dfe62ee7
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-03-27 22:45:19 +0530

    changes

--- first-extension v0.5/background.js
+++ first-extension v0.5/background.js
@@ -1,4 +1,11 @@
-window.addEventListener("load", clickHandler);
+
+// window.addEventListener("load", clickHandler);
+
+/*setTimeout(function(){
+	clickHandler()
+},2000);
+
+
 
 function clickHandler () {
 	chrome.notifications.create('',{
@@ -8,3 +15,33 @@ function clickHandler () {
 		iconUrl: "icon-48.png"
 	},function(){});
 }
+*/
+
+chrome.browserAction.setTitle({title:'Remind Me'});
+chrome.browserAction.setBadgeText({text:'12'});
+chrome.browserAction.setBadgeBackgroundColor({color:[100, 100, 150, 255]});
+
+// script to bg page communication method 1
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse){
+        if(request.msg == "startFunc") {
+        	func();
+        }
+    }
+);
+
+var func = function(){
+    chrome.notifications.create('',{
+		type: "basic",
+		title: "PriceRadar",
+		message: "Item added successfully from poup to bg",
+		iconUrl: "icon-48.png"
+	},function(){});
+};
+// script to bg page communication method 1
+
+// script to bg page communication method 2
+function test(func) {
+	func.apply(this, ['foo','bar']);
+}
+// script to bg page communication method 2
